623d39ae87494100c6e31b3da6c140b54f4f1795
hideki kuno <hidekuno@gmail.com>
2023-12-11 17:50:56 +0900

    forgot to upload

---
+++ txt_tool.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#
+# This is a personal tool what I need for my working.
+#
+# hidekuno@gmail.com
+#
+import sys
+import os
+import argparse
+
+def check_words_length(s):
+    dq_mode = False
+    sp_mode = False
+    pc = None
+    words = ""
+
+    def print_result():
+        print(f'"{words}"', ":", len(words))
+
+    for c in s:
+        if dq_mode:
+            if c == '"' and pc != '\\':
+                print_result()
+                words = ""
+                dq_mode = False
+            else:
+                words += c
+        elif sp_mode:
+            if c == ' ':
+                print_result()
+                words = ""
+                sp_mode = False
+            else:
+                words += c
+        else:
+            if c == '"' and pc != '\\':
+                dq_mode = True
+            elif (pc == ' ' or not pc) and c != ' ':
+                sp_mode = True
+                words += c
+        pc = c
+
+    if sp_mode:
+        print_result()
+
+# test code
+#check_words_length('"abc def"  hoge     foo  "ghi jkl" "\\"')
+#check_words_length('12345 "abc def"    67890')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filename", type=str, dest="filename", required=True)
+    args = parser.parse_args(sys.argv[1:])
+
+    if not os.path.exists(args.filename):
+        sys.exit("No such filename")
+
+    with open(args.filename) as fd:
+        check_words_length(fd.read())
