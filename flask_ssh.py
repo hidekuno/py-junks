c38554827c7dc85366a806eee0b23f3e4d3c038d
hideki kuno <hidekuno@gmail.com>
2024-12-06 11:29:24 +0900

    update on flask_ssh.py

--- flask_ssh.py
+++ flask_ssh.py
@@ -171,4 +171,4 @@ if __name__ == '__main__':
     if args.local:
         on_local = True
 
-    app.run(debug=True, host='0.0.0.0', port=8000,)
+    app.run(debug=True, host='0.0.0.0', port=8000, threaded=True)
